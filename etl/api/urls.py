b9d9ed9be4bff8f51cd9fa53ea55dc519bec2dd9
omartounsi7 <62721212+omartounsi7@users.noreply.github.com>
2023-07-22 23:06:29 +0100

    Implement API endpoint for CSV file upload and storage

--- etl/api/urls.py
+++ etl/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.sayHello),
+    path('upload/', views.upload_csv, name='upload_csv'),
 ]
--- etl/api/views.py
+++ etl/api/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import CsvFileSerializer
+
 from django.http import HttpResponse
 
 # Create your views here.
 def sayHello(request):
     return HttpResponse("Hello World!")
+
+@api_view(['POST'])
+def upload_csv(request):
+    serializer = CsvFileSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
