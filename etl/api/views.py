cdc126f4e0ee1861e7a12a69c2c24dfd87199fdf
omartounsi7 <62721212+omartounsi7@users.noreply.github.com>
2023-07-23 18:23:03 +0100

    Implement backend for basic arithmetic transform on singular data fields

--- etl/api/views.py
+++ etl/api/views.py
@@ -12,6 +12,89 @@ from django.http import HttpResponse
 def sayHello(request):
     return HttpResponse("Hello World!")
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
+def get_field_value(csv_data, row, col):
+    rows = csv_data.strip().split('\r\n')
+    if row < 1 or row >= len(rows) or col < 1:
+        raise ValueError("Invalid coordinates.")
+    
+    columns = rows[row - 1].split(',')
+    if col >= len(columns):
+        raise ValueError("Invalid coordinates.")
+
+    return columns[col - 1]
+
+@api_view(['POST'])
+def transform_csv_field(request):
+    if request.method == 'POST':
+        # Get the coordinates and number from the request
+        row_str = request.data.get('row', None)
+        col_str = request.data.get('col', None)
+        number_str = request.data.get('number', None)
+        op = request.data.get('op', None)
+
+        # Check if all the required parameters are present
+        if row_str is None or col_str is None or number_str is None or op is None:
+            return Response({"error": "Missing parameters. Please provide row, col, op and number."}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            row = int(row_str)
+            col = int(col_str)
+            number = float(number_str)
+            
+            # Fetch the latest CSV data
+            csv_data_obj = CsvData.objects.last()
+            if not csv_data_obj:
+                return Response({"error": "No CSV data found."}, status=status.HTTP_404_NOT_FOUND)
+
+            csv_data = csv_data_obj.csv_data
+
+            # Validate the field at the given coordinates (A4) contains a number
+            if not is_number(get_field_value(csv_data, row, col)):
+                return Response({"error": "The field at the given coordinates does not contain a number."}, status=status.HTTP_400_BAD_REQUEST)
+
+            # Perform the addition operation
+            rows = csv_data.strip().split('\r\n')
+            columns = rows[row - 1].split(',')
+            current_value = float(columns[col - 1])
+
+            if op == "add":
+                new_value =  current_value + float(number)
+            elif op == "sub":
+                new_value =  current_value - float(number)
+            elif op == "mul":
+                new_value =  current_value * float(number)
+            elif op == "div":
+                new_value =  current_value / float(number)
+            else:
+                return Response({"error": "Invalid operation."}, status=status.HTTP_400_BAD_REQUEST)
+
+            columns[col - 1] = str(new_value)
+            rows[row - 1] = ','.join(columns)
+            csv_data = '\r\n'.join(rows)
+
+            # Save the updated CSV data
+            csv_data_obj.csv_data = csv_data
+            csv_data_obj.save()
+
+            # Return the updated CSV data
+            data = {'csv_data': csv_data}
+            return Response(data, status=status.HTTP_200_OK)
+
+        except ValueError as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
 
 @api_view(['GET'])
 def get_csv(request):
